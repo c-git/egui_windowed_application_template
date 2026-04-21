0698b2d841245cc6ff260a3927b93f8487a2695d
One <43485962+c-git@users.noreply.github.com>
2026-04-21 19:23:40 -0400

    feat: set more practical default size

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() -> eframe::Result<()> {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
+            .with_inner_size([1024.0, 786.0])
             .with_min_inner_size([300.0, 220.0])
             .with_icon(
                 eframe::icon_data::from_png_bytes(
