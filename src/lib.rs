1fb0d9c089c287031c89458ec4e78cab9dca24e1
One <43485962+c-git@users.noreply.github.com>
2026-04-21 02:16:09 -0400

    chore: address unused create warning

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,11 @@
 #![cfg_attr(doc, doc = include_str!("../README.md"))]
 #![warn(clippy::all, rust_2018_idioms)]
 
+#[cfg(target_arch = "wasm32")]
+mod used_in_main {
+    use wasm_bindgen_futures as _;
+    use web_sys as _;
+}
+
 mod app;
 pub use app::TemplateApp;
