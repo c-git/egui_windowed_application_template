ce3463558f45eaf2fe83d8cfdc614ce6ea52f0c2
Onè <43485962+c-git@users.noreply.github.com>
2026-04-23 18:48:10 -0400

    Clean up crate attributes in lib.rs
    This has been moved to the `Cargo.toml`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-#![warn(unused_crate_dependencies)]
-#![cfg_attr(docsrs, feature(doc_cfg))]
-#![forbid(unsafe_code)]
-#![cfg_attr(doc, doc = include_str!("../README.md"))]
 #![warn(clippy::all, rust_2018_idioms)]
 
 #[cfg(target_arch = "wasm32")]
